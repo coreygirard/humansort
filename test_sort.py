9babf57aa2d4bdcc501f61ac8af1cc2e08219fe3
Corey <corey.r.girard@gmail.com>
2018-03-19 12:30:53 -0400

    Hypothesis

--- test_sort.py
+++ test_sort.py
@@ -1,5 +1,5 @@
 from hypothesis import given
-from hypothesis.strategies import text, integers, from_regex, lists
+from hypothesis.strategies import text, integers, from_regex, lists, tuples
 import unittest
 import doctest
 import random
@@ -13,24 +13,38 @@ class TestSort(unittest.TestCase):
         self.assertEqual(data, sort.sort(temp))
 
 
+
+def gen_test_case(a, b, c):
+    if len(a) == 0:
+        i = 0
+    else:
+        i = b%(len(a)+1)
+
+    strings = []
+    for n in sorted(c):
+        temp = list(a)
+        temp.insert(i, str(n))
+        strings.append(''.join(temp))
+
+    return strings
+
+def gen_test_data(data):
+    temp = []
+    data = sorted(data, key=lambda x: x[0])
+    for a, b, c in data:
+        temp.extend(gen_test_case(a, b, c))
+
+    return temp
+
+strat = from_regex(r'\A[a-z]+\Z'), integers(), lists(integers(min_value=0))
 class TestSorting(unittest.TestCase):
-    @given(from_regex(r'\A[a-z]*\Z'), integers(), lists(integers(min_value=0)))
-    def test_sort(self, a, b, c):
-        if len(a) == 0:
-            i = 0
-        else:
-            i = b%(len(a)+1)
+    @given(lists(tuples(*strat)))
+    def test_sort(self, e):
+        strings = gen_test_data(e)
+        backup = strings[:]
+        random.shuffle(strings)
 
-        strings = []
-
-        for n in sorted(c):
-            temp = list(a)
-            temp.insert(i, str(n))
-            strings.append(''.join(temp))
-            print(strings[-1])
-
-        print('\n\n\n')
-        #self.assertEqual(decode(encode(s)), s)
+        self.assertEqual(backup, sort.sort(strings))
 
 
 def load_tests(loader, tests, ignore):
